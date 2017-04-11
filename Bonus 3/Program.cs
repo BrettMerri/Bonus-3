3aee1452d9644f2d93e1a34c766e6e321f091f02
BrettMerri <the.b.merri@gmail.com>
2017-04-11 16:16:03 -0400

    initial commit

---
+++ Bonus 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Guess a Number Game!";
+            Console.WriteLine("Welcome to the Guass a Number Game!");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++\n");
+
+            Console.WriteLine("I'm thinking of aw number between 1 and 100.");
+            Console.WriteLine("Try to guess it, n00b.\n");
+
+            Console.Write("Enter your lousy guess: ");
+            string guessString = Console.ReadLine();
+
+            int guess = isInteger(guessString);
+            if (guess == -1)
+            {
+                return;
+            }
+
+            Console.WriteLine(guess);
+        }
+
+        static public int isInteger(string guessString)
+        {
+            int guess;
+            if (!int.TryParse(guessString, out guess))
+            {
+                Console.WriteLine("Error: Input is not an integer");
+                return -1;
+            }
+            return guess;
+        }
+    }
+}
