68927ada0e2c5dde1e8ebef3b213a33a13b6d518
BrettMerri <the.b.merri@gmail.com>
2017-04-12 13:35:14 -0400

    added checkGuess method that checks if the guess is too high or too low

--- Bonus 3/Program.cs
+++ Bonus 3/Program.cs
@@ -15,45 +15,66 @@ namespace Bonus_3
             Console.WriteLine("+++++++++++++++++++++++++++++++++++\n");
 
             bool run = true;
+            int guessAmount = 0;
+            Random rnd = new Random();
 
-            while (run)
+            while (true) //infinite loop
             {
-                Random randomNumber = new Random();
-                int random = randomNumber.Next(1, 100);
+                int random = rnd.Next(1, 100);
                 Console.WriteLine("I'm thinking of aw number between 1 and 100.");
                 Console.WriteLine("Try to guess it, n00b.\n");
-
-                Console.Write("Enter your lousy guess: ");
-                string guessString = Console.ReadLine();
-
-                int guess = isValidInteger(guessString);
-                if (guess == -1)
+                while (run)
                 {
-                    return;
+                    Console.Write("Enter your lousy guess: ");
+                    string guessString = Console.ReadLine();
+
+                    int guess = isValidInteger(guessString);
+                    if (guess == -1)
+                    {
+                        continue;
+                    }
+
+                    if (checkGuess(guess, random, guessAmount))
+                    {
+                        Console.Write("Try again? (y/n): ");
+                    }
                 }
-                if (guess == random)
-                {
-                    Console.WriteLine("You must be seriously amazing slash telepathic!\n!");
-                    Console.Write("Try again? (y/n): ");
-                }
-                Console.WriteLine(guess);
             }
         }
 
-        static public int isValidInteger(string guessString)
+        public static int isValidInteger(string guessString)
         {
             int guess;
             if (!int.TryParse(guessString, out guess))
             {
-                Console.WriteLine("Error: Input is not an integer");
+                Console.WriteLine("Error: Input is not an integer\n");
                 return -1;
             }
             if (guess < 1 || guess > 100)
             {
-                Console.WriteLine("Error: Input is not between 1 and 100");
+                Console.WriteLine("Error: Input is not between 1 and 100\n");
                 return -1;
             }
             return guess;
+        }
+        public static bool checkGuess(int guess, int random, int guessAmount)
+        {
+            guessAmount++;
+            if (guess == random)
+            {
+                Console.WriteLine("You must be seriously amazing slash telepathic!\n");
+                return true;
+            }
+            else if (guess > random)
+            {
+                Console.WriteLine("Too high! Try again.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Too low, joe. Try again.");
+                return false;
+            }
         }
     }
 }
